2e001f1a4f57b326ffce7a90482c22dcd9620fb2
Nate Houk <nate.houk@gmail.com>
2021-01-06 19:49:05 +0100

    Add tests.

--- features/steps/tuples.py
+++ features/steps/tuples.py
@@ -8,22 +8,22 @@ def step_impl(context):
 
 @then(u'a.x = 4.3')
 def step_impl(context):
-    assert context.a.x is 4.3
+    assert context.a.x == 4.3
 
 
 @then(u'a.y = -4.2')
 def step_impl(context):
-    assert context.a.y is -4.2
+    assert context.a.y == -4.2
 
 
 @then(u'a.z = 3.1')
 def step_impl(context):
-    assert context.a.z is 3.1
+    assert context.a.z == 3.1
 
 
 @then(u'a.w = 1.0')
 def step_impl(context):
-    assert context.a.w is 1.0
+    assert context.a.w == 1.0
 
 
 @then(u'a is a point')
@@ -43,7 +43,7 @@ def step_impl(context):
 
 @then(u'a.w = 0.0')
 def step_impl(context):
-    assert context.a.w is 0.0
+    assert context.a.w == 0.0
 
 
 @then(u'a is not a point')
