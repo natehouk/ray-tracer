d996a8d504753389a4d74119273f736ad2889328
Nate Houk <nate.houk@gmail.com>
2021-01-10 12:47:11 +0100

    Complete matrices chapter

--- features/steps/matrices_steps.py
+++ features/steps/matrices_steps.py
@@ -364,39 +364,39 @@ def step_impl(context):
 
 @given(u'the following 4x4 matrix A15')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the following 4x4 matrix A15')
+    context.A15 = matrix(8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4)
 
 
 @then(u'inverse(A15) is the following 4x4 matrix')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then inverse(A15) is the following 4x4 matrix')
+    assert inverse(context.A15) == matrix(-0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.43590, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308)
 
 
 @given(u'the following 4x4 matrix A16')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the following 4x4 matrix A16')
+    context.A16 = matrix(9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2)
 
 
 @then(u'inverse(A16) is the following 4x4 matrix')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then inverse(A16) is the following 4x4 matrix')
+    assert inverse(context.A16) == matrix(-0.04074, -0.07778, 0.14444, -0.22222, -0.07778, 0.03333, 0.36667, -0.33333, -0.02901, -0.14630, -0.10926, 0.12963, 0.17778, 0.06667, -0.26667, 0.33333)
 
 
 @given(u'the following 4x4 matrix A17')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the following 4x4 matrix A17')
+    context.A17 = matrix(3, -9, 7, 3, 3, -8, 2, -9, -4, 4, 4, 1, -6, 5, -1, 1)
 
 
 @given(u'the following 4x4 matrix B')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given the following 4x4 matrix B')
+    context.B = matrix(8, 2, 2, 2, 3, -1, 7, 0, 7, 0, 5, 4, 6, -2, 0, 5)
 
 
 @given(u'C ← A17 * B')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given C ← A17 * B')
+    context.C = context.A17 * context.B
 
 
 @then(u'C * inverse(B) = A17')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then C * inverse(B) = A17')
+    assert context.C * inverse(context.B) == context.A17
