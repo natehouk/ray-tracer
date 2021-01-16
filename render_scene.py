9c97ae401cb36e377f5f7f32267d2f9e680ca6c6
Nate Houk <nate.houk@gmail.com>
2021-01-16 18:38:45 +0100

    Refactor

--- render_scene.py
+++ render_scene.py
@@ -21,17 +21,17 @@ def main():
     floor.transform = scaling(10, 0.01, 10)
     floor.material.color = color(1, 0.9, 0.9)
     floor.material.specular = 0
-    w.objects.insert(0, floor)
+    w.objects.append(floor)
 
     left_wall = sphere()
     left_wall.transform = translation(0, 0, 5) * rotation_y(-pi/4) * rotation_x(pi/2) * scaling(10, 0.01, 10)
     left_wall.material = floor.material
-    w.objects.insert(0, left_wall)
+    w.objects.append(left_wall)
 
     right_wall = sphere()
     right_wall.transform = translation(0, 0, 5) * rotation_y(pi/4) * rotation_x(pi/2) * scaling(10, 0.01, 10)
     right_wall.material = floor.material
-    #w.objects.insert(0, right_wall)
+    w.objects.append(right_wall)
 
     middle = sphere()
     middle.transform = translation(-0.5, 1, 0.5)
@@ -39,7 +39,7 @@ def main():
     middle.material.color = color(0.1, 1, 0.5)
     middle.material.diffuse = 0.7
     middle.material.specular = 0.3
-    #w.objects.insert(0, middle)
+    w.objects.append(middle)
 
     right = sphere()
     right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
@@ -47,7 +47,7 @@ def main():
     right.material.color = color(0.5, 1, 0.1)
     right.material.diffuse = 0.7
     right.material.specular = 0.3
-    #w.objects.insert(0, right)
+    w.objects.append(right)
 
     left = sphere()
     left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
@@ -55,7 +55,7 @@ def main():
     left.material.color = color(1, 0.8, 0.1)
     left.material.diffuse = 0.7
     left.material.specular = 0.3
-    #w.objects.insert(0, left)
+    w.objects.append(left)
 
     cam = camera(200, 100, pi/3)
     cam.transform = view_transform(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0))
