ef65a4fb2662d567360b76be9d3abe1507331bc4
Nate Houk <nate.houk@gmail.com>
2021-02-07 17:49:58 +0100

    Implement obj file tests

--- features/steps/obj_file_steps.py
+++ features/steps/obj_file_steps.py
@@ -45,3 +45,48 @@ def step_impl(context):
 @then(u'parser.vertices[4] = point(1, 1, 0)')
 def step_impl(context):
     context.parser.vertices[4] = point(1, 1, 0)
+
+
+@when(u'g ← parser.default_group')
+def step_impl(context):
+    context.g = context.parser.default_group
+
+
+@when(u't1 ← first child of g')
+def step_impl(context):
+    context.t1 = context.g[0]
+
+
+@when(u't2 ← second child of g')
+def step_impl(context):
+    context.t2 = context.g[1]
+
+
+@then(u't1.p1 = parser.vertices[1]')
+def step_impl(context):
+    assert context.t1.p1 == context.parser.vertices[1]
+
+
+@then(u't1.p2 = parser.vertices[2]')
+def step_impl(context):
+    assert context.t1.p2 == context.parser.vertices[2]
+
+
+@then(u't1.p3 = parser.vertices[3]')
+def step_impl(context):
+    assert context.t1.p3 == context.parser.vertices[3]
+
+
+@then(u't2.p1 = parser.vertices[1]')
+def step_impl(context):
+    assert context.t2.p1 == context.parser.vertices[1]
+
+
+@then(u't2.p2 = parser.vertices[3]')
+def step_impl(context):
+    assert context.t2.p2 == context.parser.vertices[3]
+
+
+@then(u't2.p3 = parser.vertices[4]')
+def step_impl(context):
+    assert context.t2.p3 == context.parser.vertices[4]
--- parser.py
+++ parser.py
@@ -3,11 +3,16 @@ from tuple import point
 def parse_obj_file(file):
     p = parser()
     for line in file.split("\n"):
-        if line[0] != "v" and line[0] != "f":
+        if line == "" or (line[0] != "v" and line[0] != "f"):
             p.ignored += 1
         elif line[0] == "v":
             v = line.split(" ")
-            p.vertices.append(point(v[1], v[2], v[3]))
+            p.vertices.append(point(float(v[1]), float(v[2]), float(v[3])))
+        elif line[0] == "f":
+            f = line.split(" ")
+            p.default_group.append(face(p.vertices[int(f[1])],
+                                        p.vertices[int(f[2])],
+                                        p.vertices[int(f[3])]))
     return p
 
 class parser:
@@ -16,3 +21,11 @@ class parser:
         self.ignored = 0
         self.vertices = []
         self.vertices.append(point(0, 0, 0))
+        self.default_group = []
+
+class face:
+
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
